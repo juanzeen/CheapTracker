217850374dbf8ccf8e4dbee9096564557a1bef2c
viniciushissa <vini16.hissa@gmail.com>
2025-10-12 14:55:59 -0300

    feat: User CRUD has been created. fix: Unique attributes included in models-migration

--- project/app/cruds/user_crud.py
+++ project/app/cruds/user_crud.py
@@ -1,23 +1,49 @@
 from ..models import User
+from django.contrib.auth.hashers import check_password
 
 class UserCrud:
     @staticmethod
-    def create(role, name, age, email, password_hash):
+    def create(role, name, age, email, password):
+        if len(password) < 8:
+            raise ValueError("The password must contain at least 8 characters")
+        
         return User.objects.create(role=role, name=name, age=age, email=email,
-                                   password_hash=password_hash)
+                                   password_hash=password)
     
     @staticmethod
     def read():
         return User.objects.all()
     
     @staticmethod
+    def read_email(user_email):
+        return User.objects.filter(email=user_email)
+    
+    @staticmethod
     def update(user_id, **kwargs):
         user = User.objects.get(id=user_id)
         for key, value in kwargs.items():
+            if key == "password_hash":
+                raise KeyError("Change password denied")
+
             setattr(user, key, value)
+
         user.save()
         return user
     
     @staticmethod
     def delete(user_id):
         User.objects.get(id=user_id).delete()
+
+    @staticmethod
+    def change_password(user_id, old_password, new_password):
+        user = User.objects.get(id=user_id)
+
+        if not check_password(old_password, user.password_hash):
+            raise ValueError("Incorrect password!")
+        
+        if len(new_password) < 8:
+            raise ValueError("The new password must contain at least 8 characters")
+
+        user.password_hash = new_password
+        user.save()
+        return user
