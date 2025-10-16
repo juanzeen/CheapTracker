ef0b9e2b648dd8805e419d040142a7d6984a1395
viniciushissa <vini16.hissa@gmail.com>
2025-10-16 11:09:26 -0300

    feat: CRUDs carrier and depot has been created / fix: CRUD store delete method fixed

--- project/app/cruds/carrier_crud.py
+++ project/app/cruds/carrier_crud.py
@@ -1,34 +1,34 @@
-from ..models import Store
+from ..models import Carrier
 from .address_crud import AddressCrud
 from .user_crud import UserCrud
 
-class StoreCrud:
+class CarrierCrud:
     @staticmethod
     def create(user_email, name, contact, registration, 
                street, number, complement, neighborhood, city, state, cep, country):
         user = UserCrud.read_by_email(user_email)
-        if user.role != "Shop":
-            raise ValueError("To create a Store, the user role must be: Shop")
+        if user.role != "Carr":
+            raise ValueError("To create a Carrier, the user role must be: Carr")
 
         address = AddressCrud.create(street, number, complement, 
                                         neighborhood, city, state, cep, country)
 
-        return Store.objects.create(user=user, name=name, address=address,
+        return Carrier.objects.create(user=user, name=name, address=address,
                                     contact=contact, registration=registration)
 
     @staticmethod
     def read():
-        return Store.objects.all()
+        return Carrier.objects.all()
     
     @staticmethod
-    def read_stores_by_email(user_email):
+    def read_carriers_by_email(user_email):
         user = UserCrud.read_by_email(user_email)
-        return Store.objects.filter(user=user)
+        return Carrier.objects.filter(user=user)
     
     @staticmethod
-    def update(store_id, **kwargs):
-        store = Store.objects.get(id=store_id)
-        address = store.address
+    def update(carrier_id, **kwargs):
+        carrier = Carrier.objects.get(id=carrier_id)
+        address = carrier.address
         for key, value in kwargs.items():
             if key in ["street", "number", "complement", "neighborhood", "city", 
                        "state", "cep", "country"]:
@@ -36,13 +36,13 @@ class StoreCrud:
             elif key == "user":
                 raise KeyError("Update user denied")
             else:
-                setattr(store, key, value)
+                setattr(carrier, key, value)
         
         address.save()
-        store.save()
-        return store
+        carrier.save()
+        return carrier
     
     @staticmethod
-    def delete(store_id):
-        store = Store.objects.get(id=store_id)
-        AddressCrud.delete(store.address.id)
+    def delete(carrier_id):
+        carrier = Carrier.objects.get(id=carrier_id)
+        AddressCrud.delete(carrier.address.id)
