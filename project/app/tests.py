911d8b466eff30972283b87f5b6b7fd8e8b6162e
juanzeen <jziignerdev@gmail.com>
2025-10-17 12:40:46 -0300

    feat: requirements file created and db connect test

--- project/app/tests.py
+++ project/app/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.db import connection
+from django.db.utils import OperationalError
 
-# Create your tests here.
+class DatabaseConnectionTest(TestCase):
+  "Testes para verificar conexão com o banco de dados."
+  def test_database_connection(self):
+    "Testa se a conexão com o banco de dados é bem-sucedida"
+    db_conn = connection
+    try:
+      db_conn.cursor()
+    except OperationalError:
+        connected = False
+    else:
+        connected = True
+    self.assertTrue(connected, "Conexão com o banco falhou")
