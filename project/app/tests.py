200f68f8a8def278c0b7e43c71ea72ade7f11e96
juanzeen <jziignerdev@gmail.com>
2025-10-21 23:07:58 -0300

    feat: added basic user crud tests to tests.py

--- project/app/tests.py
+++ project/app/tests.py
@@ -1,16 +1,19 @@
 from django.test import TestCase
-from django.db import connection
-from django.db.utils import OperationalError
+from app.cruds.user_crud import UserCrud
 
-class DatabaseConnectionTest(TestCase):
-  "Testes para verificar conexão com o banco de dados."
-  def test_database_connection(self):
-    "Testa se a conexão com o banco de dados é bem-sucedida"
-    db_conn = connection
-    try:
-      db_conn.cursor()
-    except OperationalError:
-        connected = False
-    else:
-        connected = True
-    self.assertTrue(connected, "Conexão com o banco falhou")
+class BasicUserCrudTest(TestCase):
+  "Tests to check users CRUD"
+  def setUp(self):
+    UserCrud.create(role="Shop", name="User Test", age=20, email="emaildeexemplo@gmail.com", password="8charminpass")
+    return super().setUp()
+
+  def test_create_user_and_get_by_email(self):
+    """ Testing if the users has been created correctly """
+    UserCrud.create(role="Shop", name="User Test", age=20, email="testedecriacao@gmail.com", password="8charminpass")
+    user = UserCrud.read_by_email("testedecriacao@gmail.com")
+    self.assertEqual(user.name, "User Test")
+
+  def test_retriver_users(self):
+    """Testing if the user has been retrieved by use crud"""
+    users = UserCrud.read()
+    self.assertGreater(len(users), 0)
