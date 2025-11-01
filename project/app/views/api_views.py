34ef2159a63e37c23abc33dc2f590d14078fb642
viniciushissa <vini16.hissa@gmail.com>
2025-11-01 14:02:44 -0300

    feat: Address View has been created

--- project/app/views/api_views.py
+++ project/app/views/api_views.py
@@ -1,8 +1,9 @@
 import json
 from django.http import JsonResponse
 from django.views import View
-from app.models import Usuario
+from app.models import Usuario, Address
 from app.cruds.user_crud import UserCrud
+from app.cruds.address_crud import AddressCrud
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth import authenticate, login, logout
@@ -96,3 +97,48 @@ class ChangePasswordView(AuthBaseView):
       return self.SuccessJsonResponse("Password successfully changed!", {"new_password": data.get('new_password')})
     except ValueError as e:
       return self.ErrorJsonResponse("Invalid old password!")
+
+@method_decorator(csrf_exempt, name='dispatch')
+class AddressesAPIView(AuthBaseView):
+  def post(self, request, *args, **kwargs):
+      try:
+        data = json.loads(request.body)
+        AddressCrud.create(data["street"], data["number"], data["complement"], data["neighborhood"], data["city"], data["state"], data["cep"], data["country"])
+        return self.SuccessJsonResponse("Address successfully created", {"street": data.get('street'), "number": data.get('number'), "complement": data.get('complement'), "neighborhood": data.get('neighborhood'), "city": data.get('city'), "state": data.get('state'), "cep": data.get('cep'), "country": data.get('country')}, 201)
+      except KeyError as e:
+        return self.ErrorJsonResponse(e.args)
+      
+  def get(self, request, *args, **kwargs):
+    addresses = AddressCrud.read().values()
+    if not addresses:
+      return self.ErrorJsonResponse("Addresses not founded!")
+
+    return self.SuccessJsonResponse('Addresses successfully retrieved!',list(addresses))
+
+@method_decorator(csrf_exempt, name='dispatch')
+class AddressAPIView(AuthBaseView):
+    def get(self, request, *args, **kwargs):
+      try:
+        address = Address.objects.get(id=kwargs['id'])
+        return self.SuccessJsonResponse("Address successfully retrieved!",{"street": address.street, "number": address.number, "complement": address.complement, "neighborhood": address.neighborhood, "city": address.city, "state": address.state, "cep": address.cep, "country": address.country}, 200)
+      except Address.DoesNotExist:
+        return self.ErrorJsonResponse("Address not found", 404)
+
+    def put(self, request, *args, **kwargs):
+      try:
+        data = json.loads(request.body)
+        address = Address.objects.get(id=kwargs['id'])
+        AddressCrud.update(address.id, **data)
+        return self.SuccessJsonResponse({"message": "Address successfully updated!", "data": data })
+      except KeyError as e:
+        return self.ErrorJsonResponse(e.args)
+      except Address.DoesNotExist:
+        return self.ErrorJsonResponse("Address not found")
+    
+    def delete(self, request, *args, **kwargs):
+      try:
+        address = Address.objects.get(id=kwargs['id'])
+        AddressCrud.delete(address.id)
+        return self.SuccessJsonResponse("Address successfully deleted!")
+      except Address.DoesNotExist:
+        return self.ErrorJsonResponse("Address not found", 404)
