b59348008d211ff39d6e9b6c283c177650e8c3aa
juanzeen <jziignerdev@gmail.com>
2025-11-18 00:16:52 -0300

    feat: basic orders views and all order routes created

---
+++ project/app/views/api/order_views.py
@@ -0,0 +1,41 @@
+import json
+from .base_views import AuthBaseView
+from app.cruds.order_crud import OrderCrud
+from app.cruds.store_crud import StoreCrud
+from django.forms.models import model_to_dict
+
+
+
+class OrdersApiView(AuthBaseView):
+  def get(self, request, *args, **kwargs):
+    orders = OrderCrud.read().values()
+    if not orders:
+      return self.ErrorJsonResponse("Orders not founded!")
+
+    return self.SuccessJsonResponse("Orders successfully retrieved!", list(orders))
+
+
+  def post(self, request, *args, **kwargs):
+    store_id = json.loads(request.body).get('store_id')
+    current_store = StoreCrud.read_by_id(store_id)
+    if not current_store:
+      return self.ErrorJsonResponse("Store not founded!", 404)
+    OrderCrud.create(store_id)
+    return self.SuccessJsonResponse("Order successfully created!", 201)
+
+
+class OrderApiView(AuthBaseView):
+  def get(self, request, *args, **kwargs):
+      try:
+        order = OrderCrud.read_by_id(kwargs["id"])
+        return self.SuccessJsonResponse("Order successfully retrieved!", model_to_dict(order))
+      except ValueError:
+        return self.ErrorJsonResponse("Order not founded!", 404)
+
+  def delete(self, request, *args, **kwargs):
+      try:
+        order = OrderCrud.read_by_id(kwargs["id"])
+        OrderCrud.delete(order.id)
+        return self.SuccessJsonResponse("Order successfully deleted!", model_to_dict(order))
+      except ValueError:
+        return self.ErrorJsonResponse("Order not founded!", 404)
